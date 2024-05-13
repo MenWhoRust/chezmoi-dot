a77fcfc1c59830bef3bc7c33271a9718b557cc0e
Phabiann-Ross De Carvalho <prdecarvalho96@outlook.com>
2024-05-13 22:24:49 +0200

    Update .config/doom/config.el Update .config/doom/packages.el

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -10,6 +10,7 @@
 (package! centered-cursor-mode)
 (package! focus)
 (package! elcord)
+(package! catppuccin-theme)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
